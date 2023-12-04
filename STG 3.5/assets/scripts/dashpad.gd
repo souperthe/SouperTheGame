f2c8c5b8e00b2efe52f94a9fe3bb97a023c8eb00
souperthe <souperthecool@gmail.com>
2023-12-03 20:30:07 -0400

    oh my goodness tiles work!

--- STG 3.5/assets/scripts/dashpad.gd
+++ STG 3.5/assets/scripts/dashpad.gd
@@ -14,15 +14,23 @@ func _ready():
 func _process(delta):
 	if overlaps_body(objplayer):
 		if !objplayer.currentstate == ("Mach3") or !objplayer.currentstate == ("Mach2"):
-			$swash.play()
-		if not crazy:
-			objplayer.changestate("Mach2")
-		if crazy:
-			objplayer.changestate("Mach3")
-		if scale.x == 1:
-			objplayer.face = false
-			objplayer.animator.flip_h = false
-		if scale.x == -1:
-			objplayer.face = true
-			objplayer.animator.flip_h = true
+			dothing()
 	pass
+	
+func dothing():
+	$swash.play()
+	if not crazy:
+		if !objplayer.currentstate == ("Mach2") and !objplayer.currentstate == ("Mach3"):
+			objplayer.changestate("Mach2")
+		if objplayer.currentstate == ("Mach2"):
+			objplayer.changestate("Mach3")
+		if objplayer.currentstate == ("Mach3"):
+			objplayer.changestate("Mach3")
+	if crazy:
+			objplayer.changestate("Mach3")
+	if scale.x == 1:
+		objplayer.face = false
+		objplayer.animator.flip_h = false
+	if scale.x == -1:
+		objplayer.face = true
+		objplayer.animator.flip_h = true
