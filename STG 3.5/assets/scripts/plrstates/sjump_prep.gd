243016dbe746b1e2f75880114308899fec328859
null <karterbeach011@gmail.com>
2023-12-06 10:49:42 -0600

    You can now play as: InferiorLuigiQC

--- STG 3.5/assets/scripts/plrstates/sjump_prep.gd
+++ STG 3.5/assets/scripts/plrstates/sjump_prep.gd
@@ -24,15 +24,15 @@ func physics_update(delta: float) -> void:
 	if Input.is_action_pressed(player.input_left):
 		player.velocity.x = -225
 		if player.is_on_floor():
-			player.velocity.y = -player.jump_impulse / 2
-			if !player.sfxfoot.playing:
-				player.sfxfoot.play()
+			player.velocity.y = -player.jump_impulse / 4
+#			if !player.sfxfoot.playing:
+#				player.sfxfoot.play()
 	if Input.is_action_pressed(player.input_right):
 		player.velocity.x = 225
 		if player.is_on_floor():
-			player.velocity.y = -player.jump_impulse / 2
-			if !player.sfxfoot.playing:
-				player.sfxfoot.play()
+			player.velocity.y = -player.jump_impulse / 4
+#			if !player.sfxfoot.playing:
+#				player.sfxfoot.play()
 	if not Input.is_action_pressed(player.input_up) and ready == 1:
 		state_machine.transition_to("sjump_start")
 	#if not Input.is_action_pressed(player.input_up) and ready == 1:
