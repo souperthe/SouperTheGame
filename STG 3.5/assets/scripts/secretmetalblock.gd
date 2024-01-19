83fb8475495d9cc4f61ec2d8375058b22c3ed8b5
souperthe <souperthecool@gmail.com>
2024-01-18 20:39:16 -0400

    speed.. i love SPEED

--- STG 3.5/assets/scripts/secretmetalblock.gd
+++ STG 3.5/assets/scripts/secretmetalblock.gd
@@ -12,18 +12,14 @@ func _ready():
 	if (global.othersroom.has(global.targetRoom2 + name)):
 		queue_free()
 		deletetile()
-	$sprite.visible = true
-	$hitpart1.emitting = false
-	$hitpart2.emitting = false
-	$hitpart3.emitting = false
-	$hitpart4.emitting = false
+	$hitpart5.emitting = false
 	if destoryed:
 		destory()
 
 
-#func _process(delta):
-	#if overlaps_body(objplayer):
-		#destory()
+func _process(delta):
+	if is_instance_valid($sprite):
+		$sprite.visible = global.showcolloisions
 		
 func deletetile():
 	var position1 = position
@@ -41,12 +37,12 @@ func deletetile():
 	global.delete_tile_at(position3)
 	global.delete_tile_at(position4)
 func destory():
-	global.addcombo()
+	global.resetcombo()
 	$CollisionShape2D.queue_free()
 	hitpart()
 	deadsound()
 	deletetile()
-	$sprite.visible = false
+	$sprite.queue_free()
 	global.othersroom.append(global.targetRoom2 + name)
 	var t = Timer.new()
 	t.set_wait_time(4)
@@ -55,13 +51,13 @@ func destory():
 	t.start()
 	yield(t, "timeout")
 	queue_free()
+
+func kill(blahblah):
+	destory()
 	
 	
 func hitpart():
-	$hitpart1.emitting = true
-	$hitpart2.emitting = true
-	$hitpart3.emitting = true
-	$hitpart4.emitting = true
+	$hitpart5.emitting = true
 	
 func deadsound():
-	$hurt3.play()
+	$hurt4.play()
