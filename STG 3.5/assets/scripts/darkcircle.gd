c285bc5e0c70d4faa9da8789508bed3d74cef441
souperthe <souperthecool@gmail.com>
2024-05-08 21:35:17 -0300

    sdasda

--- STG 3.5/assets/scripts/darkcircle.gd
+++ STG 3.5/assets/scripts/darkcircle.gd
@@ -9,7 +9,7 @@ var canzoomin = false
 func _ready():
 	$Timer.start()
 	$CanvasLayer/Control/BackBufferCopy2/mask.scale = Vector2(30,30)
-	$CanvasLayer/Control/Light2D.energy = 0
+	$CanvasLayer/Control/Light2D.energy = 0.7
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +20,7 @@ func _process(_delta):
 	$CanvasLayer/Control/BackBufferCopy2/mask.position.y = clamp(spawn_pos.y - 85, -101, 441)
 	if canzoomin and $CanvasLayer/Control/BackBufferCopy2/mask.scale != Vector2(1,1):
 		$CanvasLayer/Control/BackBufferCopy2/mask.scale = lerp($CanvasLayer/Control/BackBufferCopy2/mask.scale, Vector2(2,2), 5 * _delta)
-		$CanvasLayer/Control/Light2D.energy = lerp($CanvasLayer/Control/Light2D.energy, 0.55, 2 * _delta)
+		$CanvasLayer/Control/Light2D.energy = lerp($CanvasLayer/Control/Light2D.energy, 0.1, 1 * _delta)
 
 
 func _on_Timer_timeout():
