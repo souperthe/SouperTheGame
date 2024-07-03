7f7789e914da4b256e338bebadb8482589d658e6
souperthe <souperthecool@gmail.com>
2024-07-03 16:40:10 -0300

    getting basic stuff working

---
+++ SouperTheGame/assets/scripts/CollideDebug.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+class_name Collideable
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$BlankBlock.visible = global.showdebug
+	pass
---
+++ SouperTheGame/assets/scripts/global.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var escape = false
+var score = 0
+var combo = 0
+var targetdoor = "A"
+var targetscene = null
+var showdebug = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func startroom():
+	pass
