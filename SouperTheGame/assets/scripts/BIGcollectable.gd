e7f143352dad8ab5c9851a624fb6f217ba7c75fd
souperthe <souperthecool@gmail.com>
2024-07-29 15:57:17 -0300

    only in ohio, oh!

---
+++ SouperTheGame/assets/scripts/BIGcollectable.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+
+func _ready():
+	if global.collectroom.has(global.targetscene + name):
+		queue_free()
+		print(name, " removed", ", in: ", global.targetscene.get_file())
+
+
+func _on_collision_body_entered(body):
+	if body is Player:
+		global.score += 100
+		global.collectroom.append(global.targetscene + name)
+		queue_free()
+	pass # Replace with function body.
