0450be3318bb165e68cccdbe5057fd0749c3312f
superthe <souperthecool@gmail.com>
2024-10-04 18:31:51 -0300

    goodbye godot... this is the end for yoou...

--- SouperTheGame/assets/scripts/CollideDebug.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-class_name Collideable
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	$BlankBlock.visible = global.showdebug
-	pass
--- SouperTheGame/assets/scripts/c_platform.gd
+++
@@ -1,5 +0,0 @@
-extends Collideable
-
-func _process(_delta):
-	$playercollision/CollisionShape2D.disabled = plr.state == plr.states.ladder
-	pass
